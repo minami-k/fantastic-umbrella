3f68e1ba0a8f6d29c07997365f21664350f4d5fd
Francois Polo <andasan@gmail.com>
2021-10-21 13:57:58 -0700

    added sign up and home page

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,54 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
+from django.db import IntegrityError
 
-# Create your views here.
+def home(request):
+    return render(request, 'todo/home.html')
+
+# AUTHENTICATION
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'todo/signup.html', { 'form' : UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            return redirect('currenttodos')
 
+            try:
+                #check
+                user = User.objects.create_user(request.POST['username'], request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(
+                    request, 
+                    'todo/signup.html', 
+                    {
+                        'form': UserCreationForm(),
+                        'error':'That username has already been taken.'
+                    }
+                )
+        else:
+            #Tell the user that passwords do not match
+            return render(
+                request, 
+                'todo/signup.html', 
+                {
+                    'form': UserCreationForm(),
+                    'error':'Passwords did not match'
+                }
+            )
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+#TODOS
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    return render(
+        request, 
+        'todo/currenttodos.html'
+        )
--- todolist/urls.py
+++ todolist/urls.py
@@ -19,9 +19,11 @@ from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 
     #auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('logout', views.logoutuser, name='logoutuser'),
 
     #todos
     path('current/', views.currenttodos, name='currenttodos'),
