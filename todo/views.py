1810ccd7827089b07f6d3c2db85b075ce14ce4d2
Francois Polo <andasan@gmail.com>
2021-10-21 14:21:59 -0700

    added login functionality

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 def home(request):
@@ -44,6 +44,29 @@ def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+    
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/login.html', { 'form' : AuthenticationForm()})
+    else:
+        user = authenticate(
+            request, 
+            username=request.POST['username'], 
+            password=request.POST['password']
+        )
+
+        if user is None:
+            return render(
+                request, 
+                'todo/login.html', 
+                { 
+                    'form' : AuthenticationForm(),
+                    'error':'Username and password are incorrect'
+                }
+            )
+        else:
+            login(request, user)
+            return redirect('currenttodos')
 
 
 #TODOS
--- todolist/urls.py
+++ todolist/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     #auth
     path('signup/', views.signupuser, name='signupuser'),
     path('logout', views.logoutuser, name='logoutuser'),
+    path('login', views.loginuser, name='loginuser'),
 
     #todos
     path('current/', views.currenttodos, name='currenttodos'),
